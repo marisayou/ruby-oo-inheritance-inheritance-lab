f1aa3d047493db4f0b3f8815eea351948c485a4f
Marisa You <marisa.you@gmail.com>
2020-10-14 11:38:14 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative 'user.rb'
+
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize
+        super
+        @knowledge = []
+    end
+    
+    def learn(str)
+
+        @knowledge << str
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
